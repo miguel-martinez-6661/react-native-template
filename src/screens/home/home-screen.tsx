1769e7482a1fdef6e1432702c5dd69131b6f6306
Miguel Martinez <mmartinez6661@gmail.com>
2023-12-01 12:30:23 -0300

    remove all redux features

--- src/screens/home/home-screen.tsx
+++ src/screens/home/home-screen.tsx
@@ -1,19 +1,13 @@
 import React, { useCallback } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigationProps } from "@/interfaces/navigation/navigation";
-import { useAppDispatch, useAppSelector } from "@/hooks";
-import { decrement, increment } from "@/store";
 import { Routes } from "@/navigation/routes";
-
 import { Button, Text } from "@/components/shared";
 import { Screen } from "@/components/shared";
 import { ButtonContainer } from "./home-screen.styled";
 
 export const HomeScreen = () => {
-  const dispatch = useAppDispatch();
   const { navigate } = useNavigation<AppNavigationProps>();
-
-  const { value } = useAppSelector((state) => state.counter);
 
   const handleNavigation = useCallback(() => {
     navigate(Routes.Details);
@@ -21,25 +15,13 @@ export const HomeScreen = () => {
 
   return (
     <Screen>
-      <Text>{`Hello Home => ${value}`}</Text>
+      <Text>{`Hello Home`}</Text>
       <ButtonContainer>
         <Button onPress={handleNavigation}>Navigate</Button>
 
-        <Button
-          onPress={() => {
-            dispatch(decrement());
-          }}
-        >
-          Decrease
-        </Button>
+        <Button onPress={() => {}}>Decrease</Button>
 
-        <Button
-          onPress={() => {
-            dispatch(increment());
-          }}
-        >
-          Increase
-        </Button>
+        <Button onPress={() => {}}>Increase</Button>
       </ButtonContainer>
     </Screen>
   );
--- src/store/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./store";
-
-export * from "./slices/counter-slice";
